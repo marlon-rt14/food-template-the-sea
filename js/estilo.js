5880f12998aa5d257c57977a6de8974be26d7155
marlon-rt14 <mjavierrt14@gmail.com>
2021-08-13 23:29:06 -0500

    join all responsive files into one

--- js/estilo.js
+++ js/estilo.js
@@ -1,10 +1,56 @@
-window.addEventListener("scroll", (e) => {
-	let nav = document.querySelector(".nav");
-	nav.style.position = "fixed";
-	const scrollY = window.scrollY;
-	if (scrollY == 0) {
-		nav.style.position = "relative";
-		nav.style.transition = `${nav.style.position} .2s`;
-	}
+window.addEventListener("load", (e) => {
+  let menuBebida = document.querySelector("#drink-menu");
+  menuBebida.style.display = "none";
+  if (window.scrollY == 0) {
+    document.querySelector(".nav").style.position = "relative";
+  }
+  const navLiRes = document.querySelectorAll(".nav__li");
+  const navLi = document.querySelectorAll(".nav__ul .nav__li");
 
+  navLiClick(navLiRes);
+});
+
+function navLiClick(lista) {
+  for (const element of lista) {
+    //   console.log(element);
+    element.addEventListener("click", (e) => {
+      let direction = element.firstElementChild.getAttribute("href");
+      window.location = direction;
+    });
+  }
+}
+
+window.addEventListener("scroll", (e) => {
+  let nav = document.querySelector(".nav");
+  const scrollY = window.scrollY;
+  if (scrollY > 0) {
+    nav.style.position = "fixed";
+  } else {
+    nav.style.position = "relative";
+  }
+});
+
+function menuClick(menuBebida) {
+  menuBebida.addEventListener("click", (e) => {
+    alert("hola bebida");
+    menuComida.style.display = "none";
+    menuBebida.style.display = "relative";
+  });
+}
+
+let bebida = document.querySelector(".drink-menu");
+let comida = document.querySelector(".food-menu");
+
+bebida.addEventListener("click", (e) => {
+    let menuComida = document.querySelector("#food-menu");
+    let menuBebida = document.querySelector("#drink-menu");
+    menuComida.style.display = "none";
+    menuBebida.style.display = "block";
+});
+
+comida.addEventListener("click", (e) => {
+    let menuComida = document.querySelector("#food-menu");
+    let menuBebida = document.querySelector("#drink-menu");
+    menuBebida.style.display = "none";
+    menuComida.style.display = "block";
 });
