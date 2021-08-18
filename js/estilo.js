42331123aba85a98d9b2a2c5e6079f9be727235c
marlon-rt14 <mjavierrt14@gmail.com>
2021-08-18 13:00:02 -0500

    remove unnecessary css files

--- js/estilo.js
+++ js/estilo.js
@@ -42,15 +42,48 @@ let bebida = document.querySelector(".drink-menu");
 let comida = document.querySelector(".food-menu");
 
 bebida.addEventListener("click", (e) => {
-    let menuComida = document.querySelector("#food-menu");
-    let menuBebida = document.querySelector("#drink-menu");
-    menuComida.style.display = "none";
-    menuBebida.style.display = "block";
+  let menuComida = document.querySelector("#food-menu");
+  let menuBebida = document.querySelector("#drink-menu");
+  menuComida.style.display = "none";
+  menuBebida.style.display = "block";
 });
 
 comida.addEventListener("click", (e) => {
-    let menuComida = document.querySelector("#food-menu");
-    let menuBebida = document.querySelector("#drink-menu");
-    menuBebida.style.display = "none";
-    menuComida.style.display = "block";
+  let menuComida = document.querySelector("#food-menu");
+  let menuBebida = document.querySelector("#drink-menu");
+  menuBebida.style.display = "none";
+  menuComida.style.display = "block";
+});
+
+let popup = document.getElementById("popup");
+const imgContainer = document.querySelectorAll(".gallery-img-container");
+for (let i = 0; i < imgContainer.length; i++) {
+  let idImageGallery = `idImageGallery${i}`;
+  imgContainer[i].firstElementChild.setAttribute("id", `${idImageGallery}`);
+  let imgGallery = document.getElementById(`${idImageGallery}`);
+  imgGallery.addEventListener("click", (e) => {
+    let modalImg = document.getElementById("modalImg");
+    popup.style.display = "block";
+    let source = imgGallery.getAttribute("src");
+    modalImg.setAttribute("src", `${source}`);
+  });
+}
+
+const span = document.querySelector(".close");
+
+span.addEventListener("click", (e) => {
+  popup.style.display = "none";
+});
+
+popup.addEventListener("mousemove", (e) => {
+  let modalImg = document.getElementById("modalImg");
+  modalImg.style.filter = "brightness(1)";
+  modalImg.style.transform = "scale(1)";
+});
+
+const modalContent = document.querySelector(".modal-content");
+modalContent.addEventListener("mouseleave", (e) => {
+  popup.addEventListener("click", (evt) => {
+    popup.style.display = "none";
+  });
 });
